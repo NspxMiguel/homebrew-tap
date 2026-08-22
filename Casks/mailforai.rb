112dfb85b5b8e37133f54675763c9902021c5bdf
Miguel <miguel@keepok.com.br>
2026-08-21 23:49:29 -0300

    Bump MailForAI cask to 0.5.1

--- Casks/mailforai.rb
+++ Casks/mailforai.rb
@@ -1,6 +1,6 @@
 cask "mailforai" do
-  version "0.5.0"
-  sha256 "e81461e81f8d56e69378344d06aeac25b20bcde913cc4cf60ffc1160bfba525f"
+  version "0.5.1"
+  sha256 "960096e3de5c426329e91eeb87f903775afedc8c37eaf2221fdff7511da90fab"
 
   # Baixa o CODIGO-FONTE e compila na maquina de quem instala. Build local =
   # sem atributo de quarentena no binario final = sem aviso de Gatekeeper, e
